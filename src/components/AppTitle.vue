c647b2e43dc8153652c38c0078efb046feb30b10
matteo-cota <matteocota@outlook.it>
2024-09-13 23:00:12 +0200

    Added AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>La mia prima app con Vite!</h1>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppTitle',
+  }
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
